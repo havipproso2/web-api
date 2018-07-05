4b8f1db902ed930cd453cc52c60b73c8abd07c8f
Vo Le Nhut Ha <volenhutha1994@gmail.com>
2018-07-05 14:18:26 +0700

    Config

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+// import auth from './routes/auth';
+// import users from './routes/users';
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+// mongoose.connect(process.env.MONGODB_URL);
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+
+
+// app.use('/api/auth', auth);
+// app.use('/api/users', users);
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(8081, () => console.log("Running on localhost:8081"));
