62f658ee8fd480dceb02f1f1cb6ad7bb2493fd77
Vo Le Nhut Ha <volenhutha1994@gmail.com>
2018-07-16 18:07:23 +0700

    Login Route

--- src/index.js
+++ src/index.js
@@ -4,19 +4,15 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
-// import auth from './routes/auth';
-// import users from './routes/users';
+import auth from './routes/auth';
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-// mongoose.connect(process.env.MONGODB_URL);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL);
 
-
-// app.use('/api/auth', auth);
-// app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
